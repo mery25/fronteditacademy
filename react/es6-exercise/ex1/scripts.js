ba86e1213396a9465fca69e500bb4013810f478d
Maria Tibau <maria.ta.89@gmail.com>
2020-11-16 16:16:55 +0100

    Added ex1

---
+++ react/es6-exercise/ex1/scripts.js
@@ -0,0 +1,12 @@
+const li = `
+<li>
+    <div class="row">
+        <div class="col-md-4">
+            <img src="${moviePoster}" height="250" alt="" />
+        </div>
+        <div class="col-md-8">
+            <h2>${movieTitle}</h2>
+        </div>
+    </div>
+</li>
+`;
