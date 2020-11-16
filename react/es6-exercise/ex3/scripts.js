59b65f8aef24359ba19e1724d7b4b9a006ea3fef
Maria Tibau <maria.ta.89@gmail.com>
2020-11-16 16:30:23 +0100

    Fixed error on ex3

---
+++ react/es6-exercise/ex3/scripts.js
@@ -0,0 +1,10 @@
+var users = 
+[{ firstName: 'Homer', lastName: 'Simpson' },
+{ firstName: 'Marge', lastName: 'Simpson' },
+{ firstName: 'Bart', lastName: 'Simpson' },
+{ firstName: 'Lisa', lastName: 'Simpson' },
+{ firstName: 'Maggie', lastName: 'Simpson' }];
+
+users.map(function (user) {
+   return user.firstName;
+});
